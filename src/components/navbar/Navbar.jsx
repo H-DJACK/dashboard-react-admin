f451179e2515a11d7bd700368a4787ce8bd26146
Christian K. Djimeli <djkachempire@gmail.com>
2022-06-22 03:35:17 +0100

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import './navbar.scss';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import ChatBubbleOutlinedIcon from '@mui/icons-material/ChatBubbleOutlined';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="search..." />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className="icon" />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlinedIcon className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img src="#" alt="user" className="avatar" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
